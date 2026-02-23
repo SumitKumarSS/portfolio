af8bb19086f7414771031e647f725fe4339a7008
Sumit Singh <sumit@maisonqhobosheane.com>
2026-02-23 10:51:59 +0530

    Initial commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Layout/Footer";
 import Hero from "@/components/Sections/Hero";
 import About from "@/components/Sections/About";
 import Skills from "@/components/Sections/Skills";
-import Projects from "@/components/Sections/Projects";
+// import Projects from "@/components/Sections/Projects";
 import Contact from "@/components/Sections/Contact";
 import ScrollHandler from "@/components/ScrollHandler";
 
